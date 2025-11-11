fbbb4fbab81b6f1b740f65c2c30c5e4bf2edd606
Lê Quốc Khánh <khanhlq4979@ut.edu.vn>
2025-11-11 21:14:10 +0700

    cập nhật client

--- Client/ClientForm.Designer.cs
+++ Client/ClientForm.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btnConnect = new Button();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
@@ -39,39 +39,44 @@
             textBox5 = new TextBox();
             button2 = new Button();
             label4 = new Label();
+            label5 = new Label();
+            textBox6 = new TextBox();
             SuspendLayout();
             // 
-            // button1
+            // btnConnect
             // 
-            button1.Location = new Point(12, 12);
-            button1.Name = "button1";
-            button1.Size = new Size(119, 48);
-            button1.TabIndex = 0;
-            button1.Text = "Connect";
-            button1.UseVisualStyleBackColor = true;
+            btnConnect.BackColor = SystemColors.Control;
+            btnConnect.Location = new Point(13, 13);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(116, 28);
+            btnConnect.TabIndex = 0;
+            btnConnect.Text = "Connect";
+            btnConnect.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(800, 63);
+            label1.Location = new Point(213, 18);
             label1.Name = "label1";
-            label1.Size = new Size(62, 20);
+            label1.Size = new Size(65, 20);
             label1.TabIndex = 1;
-            label1.Text = "Address";
+            label1.Text = "Address:";
+            label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(543, 60);
+            label2.Location = new Point(200, 59);
             label2.Name = "label2";
-            label2.Size = new Size(75, 20);
+            label2.Size = new Size(78, 20);
             label2.TabIndex = 2;
-            label2.Text = "Username";
+            label2.Text = "Username:";
+            label2.Click += label2_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(827, 15);
+            label3.Location = new Point(455, 21);
             label3.Name = "label3";
             label3.Size = new Size(35, 20);
             label3.TabIndex = 3;
@@ -80,44 +85,48 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(868, 60);
+            textBox1.Location = new Point(285, 52);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(125, 27);
             textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(624, 60);
+            textBox2.Location = new Point(285, 18);
+            textBox2.Margin = new Padding(4);
+            textBox2.MaxLength = 200;
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(125, 27);
             textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(868, 12);
+            textBox3.Location = new Point(496, 21);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(125, 27);
             textBox3.TabIndex = 6;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(47, 143);
+            textBox4.Location = new Point(12, 109);
             textBox4.Multiline = true;
             textBox4.Name = "textBox4";
             textBox4.ScrollBars = ScrollBars.Vertical;
-            textBox4.Size = new Size(908, 390);
+            textBox4.Size = new Size(568, 301);
             textBox4.TabIndex = 7;
+            textBox4.TextChanged += textBox4_TextChanged;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(47, 577);
+            textBox5.Location = new Point(12, 543);
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(804, 27);
             textBox5.TabIndex = 8;
             // 
             // button2
             // 
-            button2.Location = new Point(861, 575);
+            button2.Location = new Point(836, 452);
             button2.Name = "button2";
             button2.Size = new Size(94, 29);
             button2.TabIndex = 9;
@@ -127,18 +136,37 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(47, 554);
+            label4.Location = new Point(12, 520);
             label4.Name = "label4";
             label4.Size = new Size(110, 20);
             label4.TabIndex = 10;
             label4.Text = "Enter Messager";
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(455, 59);
+            label5.Name = "label5";
+            label5.Size = new Size(35, 20);
+            label5.TabIndex = 11;
+            label5.Text = "Port";
+            label5.Click += label5_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(496, 56);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 27);
+            textBox6.TabIndex = 12;
+            // 
             // ClientForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.GradientActiveCaption;
-            ClientSize = new Size(1005, 631);
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(1291, 695);
+            Controls.Add(textBox6);
+            Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(button2);
             Controls.Add(textBox5);
@@ -149,7 +177,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(button1);
+            Controls.Add(btnConnect);
             MaximizeBox = false;
             Name = "ClientForm";
             Text = "Client";
@@ -159,7 +187,7 @@
 
         #endregion
 
-        private Button button1;
+        private Button btnConnect;
         private Label label1;
         private Label label2;
         private Label label3;
@@ -170,5 +198,7 @@
         private TextBox textBox5;
         private Button button2;
         private Label label4;
+        private Label label5;
+        private TextBox textBox6;
     }
 }
--- Client/ClientForm.cs
+++ Client/ClientForm.cs
@@ -1,15 +1,54 @@
-namespace Client
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+public partial class ClientForm : Form
 {
-    public partial class ClientForm : Form
+    private TcpClient tcpClient;
+    private NetworkStream networkStream;
+    private StreamReader reader;
+    private StreamWriter writer;
+
+    public ClientForm()
     {
-        public ClientForm()
+        InitializeComponent();
+    }
+
+    private void btnSend_Click(object sender, EventArgs e)
+    {
+        try
         {
-            InitializeComponent();
+            string serverAddress = txtServerAddress.Text;
+            int port = int.Parse(txtPort.Text);
+
+            // Khởi tạo TCP client
+            tcpClient = new TcpClient(serverAddress, port);
+            networkStream = tcpClient.GetStream();
+            writer = new StreamWriter(networkStream, Encoding.ASCII) { AutoFlush = true };
+
+            // Gửi tin nhắn
+            string message = txtMessage.Text;
+            writer.WriteLine(message);
+
+            // Hiển thị tin nhắn đã gửi trong nhật ký chat
+            lstChatLog.AppendText("You: " + message + "\n");
+
+            // Nhận phản hồi từ server (nếu có)
+            reader = new StreamReader(networkStream, Encoding.ASCII);
+            string response = reader.ReadLine();
+            lstChatLog.AppendText("Server: " + response + "\n");
+
+            // Đóng kết nối
+            writer.Close();
+            reader.Close();
+            networkStream.Close();
+            tcpClient.Close();
         }
-
-        private void label3_Click(object sender, EventArgs e)
+        catch (Exception ex)
         {
-
+            MessageBox.Show("Lỗi: " + ex.Message);
         }
     }
 }
